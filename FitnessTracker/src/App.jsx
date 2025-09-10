f830f0f68d15e9c35c38311d7a535a2af2c5a86d
Bittu Raj <bitturaj124365@gmail.com>
2025-09-11 01:44:21 +0530

    Added heading and side drawer using Material UI

--- FitnessTracker/src/App.jsx
+++ FitnessTracker/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import FitnessApp from './Components/FitnessApp'
+import { useState } from "react";
+import "./App.css";
+// import FitnessApp from "./Components/FitnessApp";
+// import PrimarySearchAppBar from "./Pages/Dashboard";
+// import MiniDrawer from "./Pages/Drawer";
+import DrawerAndHeading from "./Pages/DrawerAndHeading";
 function App() {
   return (
-    < >
-      <FitnessApp/>
+    <>
+      <DrawerAndHeading />
+      {/* <PrimarySearchAppBar /> */}
+      {/* <MiniDrawer/> */}
+      {/* <FitnessApp /> */}
+      {/* <Dashboard /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
