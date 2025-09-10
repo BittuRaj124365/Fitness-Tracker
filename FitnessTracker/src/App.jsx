c8d7578850073cd3afc364df9c40c8e3528c67ac
Bittu Raj <bitturaj124365@gmail.com>
2025-09-10 23:27:22 +0530

    Starting to build fitness tracker website or u can say app. Created basic roadmap for the project and environments for it

---
+++ FitnessTracker/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import FitnessApp from './Components/FitnessApp'
+function App() {
+  return (
+    < >
+      <FitnessApp/>
+    </>
+  )
+}
+
+export default App
